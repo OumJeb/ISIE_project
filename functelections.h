149c3223c9222e64476c213e1136acce5577aee5
OumJeb <oumayma.jebali@esprit.tn>
2022-11-10 14:54:05 +0100

    add file

---
+++ functelections.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTELECTIONS_H_INCLUDED
+#define FUNCTELECTIONS_H_INCLUDED
+#include <stdio.h>
+typedef struct {int d;int m;int y;}date;
+typedef struct { char elecid[20];
+		 date Date;
+		 int municip;
+		 int numhab;
+		 int numps;
+		 int municipal;
+		 int legislative;
+		}election;
